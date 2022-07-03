716c621b4a78312dfdebcfbd3dcba036c3c4031e
liuyinzhe <54565139+liuyinzhe@users.noreply.github.com>
2022-07-03 22:45:43 +0800

    Update my_so2.cpp

--- 03.ctypes/my_so2.cpp
+++ 03.ctypes/my_so2.cpp
@@ -6,3 +6,6 @@ extern "C" void sort(
   double array[], const unsigned int size) {
   std::sort(array, array + size, std::less<double>());
 }
+
+// 将 C++ 程序编译成 SO 动态连结文件
+// g++ -Wall -Wextra -O -ansi -pedantic -fPIC -shared my_so2.cpp -o my_so2.so
